f7f87517755100215dd5d2a03cf065527824d8c0
Louismart <megapolice78@gmail.com>
2023-06-15 12:34:54 +0200

    OOP simple ex2

--- src/main/java/methodReferenceChallege/MainCarTest.java
+++ src/main/java/methodReferenceChallege/MainCarTest.java
@@ -3,6 +3,19 @@ package methodReferenceChallege;
 public class MainCarTest {
     public static void main(String[] args) {
 
+   /*     int x;
+        int y = 10;
+
+        x = 5;
+        x = y;
+        y = 20;
+
+
+        System.out.println("x= " + x + " " + "y= " + y);
+
+    */
+
+
         Car car1 = new Car("Audi");
         System.out.println(car1.hashCode());
 
@@ -13,16 +26,15 @@ public class MainCarTest {
         System.out.println(car1.hashCode());
         System.out.println(car2.hashCode());
 
-   //     System.out.println(car1.equals(car2));
+        //     System.out.println(car1.equals(car2));
         car1.setName("Opel");
         car2.setName("Citroen");
 
-      //  car2 = car1; //true
+        //  car2 = car1; //true
         car2.setName("Opel");
         car1.setName("Mazda");
         car1.setName("VW");
         car2.setName("VW");
-
 
 
         System.out.println(car1.equals(car2));
