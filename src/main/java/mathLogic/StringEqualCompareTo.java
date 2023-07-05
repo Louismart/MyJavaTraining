aeb42584d1ea9876d8fc8fd2e5218c5cedd74351
Louismart <megapolice78@gmail.com>
2023-07-05 12:09:07 +0200

    Small tweak

--- src/main/java/mathLogic/StringEqualCompareTo.java
+++ src/main/java/mathLogic/StringEqualCompareTo.java
@@ -7,6 +7,9 @@ public class StringEqualCompareTo {
         String str1 = "KANAVAS";
         String str2 = "KANAVAS";
 
+        String str3 = new String();
+
+
 
         System.out.println((str1.equals(str2)));  //check if the 2 Strings are equal
         System.out.println(str1.compareTo(str2)); //compare String with anotherString lexicographically
--- src/main/java/methodReferenceChallege/MainCarTest.java
+++ src/main/java/methodReferenceChallege/MainCarTest.java
@@ -17,30 +17,38 @@ public class MainCarTest {
         System.out.println("x= " + x + " " + "y= " + y);
 
     */
+        String str1 = new String("BMW");
+        String str2 = "BMW";
 
 
         Car car1 = new Car("Audi");
-        System.out.println(car1.hashCode());
+     //   System.out.println(car1.hashCode());
 
         Car car2 = new Car("Audi");
-        System.out.println(car2.hashCode());
+     //   System.out.println(car2.hashCode());
 
-//        car2=car1;
-        System.out.println(car1.hashCode());
-        System.out.println(car2.hashCode());
+        car2=car1;
+//        System.out.println(car1.hashCode());
+//        System.out.println(car2.hashCode());
 
         //     System.out.println(car1.equals(car2));
-        car1.setName("Opel");
-        car2.setName("Citroen");
+//        car1.setName("Opel");
+//        car2.setName("Citroen");
+//
+//        car2 = car1; // if the object reference is occurring , then 1 object always equals to 2 object  [true]
+//        car2.setName("Opel");
+//        car1.setName("Mazda");
+//        car1.setName("VW");
+//        car2.setName("VW");
 
-        car2 = car1; // if the object reference is occurring , then 1 object always equals to 2 object  [true]
-        car2.setName("Opel");
-        car1.setName("Mazda");
-        car1.setName("VW");
-        car2.setName("VW");
 
+//        System.out.println(car1.equals(car2));
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
 
         System.out.println(car1.equals(car2));
+        System.out.println(car1 == car2);
+
 
 
     }
